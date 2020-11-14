2190ed0400a14a4f4b246634c968856382c8cae9
Natikkk <s770@mail.ru>
2020-11-14 17:58:34 +0300

    Первые шаги Ui +++

---
+++ main1.py
@@ -0,0 +1,45 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QLabel, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
+from ui_main1 import Ui_Form
+from random import *
+
+
+
+class Ui_Form(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.flag = False
+        self.pushButton.clicked.connect(self.draw)
+
+    def draw(self):
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.flag:
+
+            qp = QPainter()
+            qp.begin(self)
+            # qp.setPen(QColor(255, 255, 0))
+            qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))  # заливка
+            x1 = randint(0, 780)
+            y1 = randint(0, 500)
+            self.x, self.y = x1, y1
+            self.drawEll(qp)
+            qp.end()
+
+    def drawEll(self, qp):
+        r1 = randint(20, 200)
+        r2 = randint(20, 200)
+        qp.drawEllipse(self.x, self.y, r1, r2)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Ui_Form()
+    ex.show()
+    sys.exit(app.exec_())
